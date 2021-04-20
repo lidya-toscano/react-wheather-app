051e6aaa05270fa152bde3457349cc0cee5cfe1a
lidya-toscano <75463460+lidya-toscano@users.noreply.github.com>
2021-04-19 19:09:10 -0700

    edited index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
 import "./App.css";
-
 import App from "./App";
-import Weather from "./Weather";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-
-import "./App.css";
-
-import App from "./App";
-import Weather from "./Weather";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
